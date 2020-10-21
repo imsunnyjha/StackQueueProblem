c24e350f770a2f339efae22fc3ea946d909b12d2
Sunny Jha <sunnyjha11897@gmail.com>
2020-10-21 22:13:22 +0530

    [SUNNY]Add.Welcome to Stack and queue implementation

---
+++ StackNQueueDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StackNQueueDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Stack and Queue Implementaion!");
+        }
+    }
+}
