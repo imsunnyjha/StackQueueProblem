1890a0d61f3c086a4d83a83d24fdbecd1d173117
Sunny Jha <sunnyjha11897@gmail.com>
2020-10-21 22:52:04 +0530

    [SUNNY]Add.UseCase3 to create queue and perform enqueue

---
+++ StackNQueueDemo/QueueOperation.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StackNQueueDemo
+{
+    class QueueOperation
+    {
+         Node head = null;
+
+        internal void Enqueue(int data)
+        {
+            Node node = new Node(data);
+            if(head == null)
+            {
+                head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;                
+            }
+            Console.WriteLine(node.data + " inserted in queue");
+        }
+        internal void Display()
+        {
+            Node temp = head;
+            if (temp == null)
+            {
+                Console.WriteLine("Empty queue");
+                return;
+            }
+            while (temp != null)
+            {
+                Console.WriteLine(temp.data+ " ");
+                temp = temp.next;
+            }
+        }
+    }
+}
